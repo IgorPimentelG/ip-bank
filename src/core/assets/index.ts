5a6485d6244539e340c6b8f5253e5272e849ffab
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-11-01 10:08:47 -0300

    🍱 style: add assets

---
+++ src/core/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as Abstract1 } from './abstract-1.png';
+export { default as Abstract2 } from './abstract-2.png';
+export { default as Abstract3 } from './abstract-3.png';
+export { default as Arrows } from './arrows.png';
+export { default as LogoDesktop } from './logo-desktop.png';
+export { default as LogoMobile } from './logo-mobile.png';
+
